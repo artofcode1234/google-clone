fa6596b301f7b81c67d6eb88816087913d92e32d
artofcode1234 <aniketmaharana448@gmail.com>
2025-05-24 12:08:24 +0530

    Add files via upload
    this is google clone

---
+++ main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.midbox');
+ScrollReveal().reveal('.more-apps');
+ScrollReveal().reveal('.offerings');
+ScrollReveal().reveal('.footer');
+
+function greet() {
+  alert('Thanks For Visiting...!')
+}
